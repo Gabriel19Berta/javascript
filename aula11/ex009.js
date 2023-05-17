ffd3b10a01b15c1b1e38284b7fc5243ef5457cf0
Gabriel <130099907+Gabriel19Berta@users.noreply.github.com>
2023-05-17 14:39:01 -0300

    Refazendo aula 11

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,7 @@
-var pais = 'França'
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
 if (pais != 'Brasil') {
-    console.log('Você é Estrangeiro!')
-}
-else {
+    console.log('Você é Estrangeiro')
+} else {
     console.log('Você é Brasileiro')
 }
